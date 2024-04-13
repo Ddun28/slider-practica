0a443ffe4bd209608ad9ae757c20d598b4563ee8
Daniel Dun <danieldum28@gmail.com>
2024-04-13 00:05:56 -0400

    add update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 import  Arrows  from "@/components/widgets/arrows";
 import  SliderItem  from "@/components/widgets/slider-item";
+import  SliderThumbnailItem from "@/components/widgets/slider-thumbnail-item";
 import { useState } from "react";
 
 export default function Home() {
 
   const [itemActive, setItemActive] = useState<number>(1);
   const countItems = 5;
+
+  const onPrev = () => {
+    setItemActive(itemActive - 1);
+  if(itemActive === 1){
+    setItemActive(countItems)
+  }
+}
+
+const onNext = ()=> {
+  setItemActive(itemActive + 1);
+  if(itemActive >= countItems){
+    setItemActive(1)
+  }
+}
 
   return (
     <div className="h-screen relative">
@@ -57,7 +72,45 @@ export default function Home() {
         "
         />
       </ul>
-      <Arrows onClickPrev={() => {}} onClickNext={() => {}} /> 
+      <Arrows onClickPrev={() => onPrev()} onClickNext={() => onNext()} /> 
+      <ul className="absolute bottom-0 z-10 flex sm:justify-end gap-3 w-full h-[250px] px-14 overflow-y-hidden overflow-x-auto">
+        <SliderThumbnailItem 
+        itemActive={itemActive} 
+        image="/imgs/1.jpg" 
+        id={1} 
+        name="Adidas" 
+        onClick={() => setItemActive(1)} 
+        />
+
+         <SliderThumbnailItem 
+        itemActive={itemActive} 
+        image="/imgs/2.jpg" 
+        id={2} 
+        name="Nike" 
+        onClick={() => setItemActive(2)}
+        />
+         <SliderThumbnailItem 
+        itemActive={itemActive} 
+        image="/imgs/3.jpg" 
+        id={3} 
+        name="Nike" 
+        onClick={() => setItemActive(2)}
+        />
+         <SliderThumbnailItem 
+        itemActive={itemActive} 
+        image="/imgs/4.jpg" 
+        id={4} 
+        name="Nike" 
+        onClick={() => setItemActive(2)}
+        />
+         <SliderThumbnailItem 
+        itemActive={itemActive} 
+        image="/imgs/5.jpg" 
+        id={5} 
+        name="Nike" 
+        onClick={() => setItemActive(2)}
+        />
+      </ul>
     </div>
   );
 }
